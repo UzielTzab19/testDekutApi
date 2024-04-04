71552bf25f6852ce20391538e3645aa7433c4d4f
Uziel Tzab <uzieltzab8@gmail.com>
2024-04-04 11:09:52 -0600

    Se corrigio el probelam anterior y ahora inserta registro con contraseñas encriptadas

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+//IMPORTACIONES DE NUESTROS MODULOS A UTILIZAR
 const express = require('express');
+const cors = require('cors');
 const mysql = require('mysql');
+const bcrypt = require('bcrypt');
 
+//INICIALIZAR LA APP CON EXPRESS PARA HACER USO DE SUS METODOS YA SEA GET, POST, PUT, DELETE Y PATCH
 const app = express();
 
+//LOS USOS DE BIBLIOTECAS QUE CONTENDRA LA APP DE EXPRESS
+app.use(cors());
 app.use(express.json());
 
+//PUERTO DE LANZAMIENTO
 const port = 3000;
 
+//CONFIGURACION PARA REALIZAR UN VINCULO CON LA BASE DE DATOS
 const dbConfig = {
     host: 'b0nwabxvlk8afpde9tdx-mysql.services.clever-cloud.com',
     user: 'ufll0clwldbfbgjc',
@@ -14,9 +22,11 @@ const dbConfig = {
     database:'b0nwabxvlk8afpde9tdx'
 };
 
-
+//CREA UNA POOL DE CONEXIONES, UNA "POOL" ES UN CONJUNTO DE CONEXIONES A LA BASE DE DATOS QUE SE PUEDEN REUTILIZAR, LO QUE PUEDE MEJORAR EL RENDIMIENTO DE LA APLICACIÓN.
+//CON ESTA NUEVA VARIABLE PODEMOS REALIZAR CONSULTAS A LA BASE DE DATOS Y CONTROLAR UNA ACCION EN BASE A SI FUE O NO EXITOSA LA CONSULTA
 const dbConnection = mysql.createPool(dbConfig);
 
+//VERIFICAMOS SI SE ESTABLECIO CORRECTAMENTE A CONEXION DE LA API A LA BASE DE DATOS TOMANDO COMO PARAMETRO LA CONFIGURACION ANTERIOR
 dbConnection.getConnection((error, connection)=>{
     if(error)
     {
@@ -26,10 +36,10 @@ dbConnection.getConnection((error, connection)=>{
     {
         console.log(`Conexion exitosa a la base de datos`);
         connection.release();
-
     }
 });
 
+//PETICION DE TIPO GET PARA TRAER A TODOS LOS JUEGOS
 app.get('/Juegos', (req, res)=>{
    dbConnection.query('SELECT * FROM Juegos', (error, results)=>{
         if(error)
@@ -44,6 +54,7 @@ app.get('/Juegos', (req, res)=>{
    })
 });
 
+//PETICION DE TIPO GET QUE TRAE A TODOS LOS USUARIOS
 app.get('/Usuarios',(req, res)=>
 {
     dbConnection.query('SELECT * FROM Usuarios', (error, results)=>{
@@ -59,46 +70,54 @@ app.get('/Usuarios',(req, res)=>
     });
 });
 
-app.get('/Usuario/:CorreoElectronico/:Contraseña',(req, res)=>{
-    const {CorreoElectronico, Contraseña} = req.params;
 
-        dbConfig.query('SELECT * FROM Usuarios WHERE CorreoElectronico = ? AND Contraseña = ?',[CorreoElectronico,Contraseña],(error, results)=>{
-            if(error)
-            {
-                console.error('Ocurrio un error al obtner especificamente a un usuario');
-                res.status(500).json({message:"Ocurrio un error al obtner especificamente a un usuario"});
-            }
-            else if(results.affectedRows === 0)
-            {
-                console.log("No se encontro el usuario deseado");
-                res.status(404).json({message: "Usuario no encontrado"});
-            }
-            else
-            {
-                console.log("Usuario encontrado exitosamente");
-                res.status(200).json(results);
-            }
-    });
+app.post('/Usuario', (req, res) => {
+  const { CorreoElectronico, Contraseña } = req.body;
+
+  dbConnection.query('SELECT * FROM Usuarios WHERE CorreoElectronico = ?', [CorreoElectronico], (error, results) => {
+    if (error) {
+      console.error('Ocurrió un error al obtener específicamente a un usuario');
+      res.status(500).json({ message: "Ocurrió un error al obtener específicamente a un usuario" });
+    } else if (results.length === 0) {
+      console.log("No se encontró el usuario deseado");
+      res.status(404).json({ message: "Usuario no encontrado" });
+    } else {
+        console.log("Contraseña proporcionada:", Contraseña);
+        console.log("Contraseña almacenada:", results[0].Contraseña);
+      bcrypt.compare(Contraseña, results[0].Contraseña, (err, result)=> {
+        if(result) {
+          console.log("Usuario encontrado exitosamente");
+          res.status(200).json(results[0]);
+        } else {
+          console.log("Contraseña incorrecta");
+          res.status(401).json({ message: "Contraseña incorrecta" });
+        }
+      });
+    }
+  });
 });
 
-app.post('/Usuario', (req, res)=>{
+app.post('/InsertUsuario', (req, res) => {
     console.log(req.body);
-    const {Nombre, CorreoElectronico, Contraseña} = req.body;
-    dbConnection.query('INSERT INTO Usuarios(Nombre, CorreoElectronico, Contraseña) VALUES (?,?,?)', [Nombre, CorreoElectronico, Contraseña],(error, results)=>
-    {
-        if(error)
-        {
-            console.error(`Ocurrio un error al querer insertar un nuevo registro en la tabla Usuarios`, error);
-            res.status(500).json({messge: "Occurio un error al querer insertar un registro en la tabla Usuarios"});
-        }
-        else
-        {
-            res.status(201).json({message: "Usuario registrado correctamente"});
+    const { Nombre, CorreoElectronico, Contraseña } = req.body;
+  
+    bcrypt.hash(Contraseña, 10, (err, hash) => {
+      if (err) {
+        console.error('Ocurrió un error al encriptar la contraseña', err);
+        res.status(500).json({ message: "Ocurrió un error al encriptar la contraseña" });
+      } else {
+        dbConnection.query('INSERT INTO Usuarios(Nombre, CorreoElectronico, Contraseña) VALUES (?,?,?)', [Nombre, CorreoElectronico, hash], (error, results) => {
+          if (error) {
+            console.error('Ocurrió un error al querer insertar un nuevo registro en la tabla Usuarios', error);
+            res.status(500).json({ message: "Ocurrió un error al querer insertar un registro en la tabla Usuarios" });
+          } else {
+            res.status(201).json({ message: "Usuario registrado correctamente" });
             res.json(results);
-        }
+          }
+        });
+      }
     });
-
-});
+  });
 
 app.post('/Juego', (req, res)=>
 {
